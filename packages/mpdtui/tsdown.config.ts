88cf4747574d14f0c96494f3db713c9e3890fab2
Michael Ostermann <michaelostermann@me.com>
2025-11-20 13:48:33 +0100

    fix: build

--- packages/mpdtui/tsdown.config.ts
+++ packages/mpdtui/tsdown.config.ts
@@ -5,6 +5,11 @@ export default defineConfig({
     dts: true,
     entry: ["./src/index.ts"],
     format: "esm",
+    noExternal: ["mpd-api"],
+    copy: {
+        from: "../../node_modules/mpd-api/lib/api/spec",
+        to: "dist/spec",
+    },
     outputOptions: {
         banner: "#!/usr/bin/env bun",
     },
